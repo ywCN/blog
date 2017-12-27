ce85eee789f4a05cc20e8dbc3e96ca5e393b1879
ywCN <wangyouhao90@gmail.com>
2017-12-26 22:20:59 -0500

    import and apply promise

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
@@ -10,7 +11,7 @@ import PostsIndex from './components/posts_index';
 // Do not need App component when using Router because 
 // we do not need a central single component.
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
