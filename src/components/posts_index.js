c874633e13689e362a39db462ca5c19f4499a493
ywCN <wangyouhao90@gmail.com>
2017-12-26 23:18:00 -0500

    comment for lifecycle method

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -3,6 +3,18 @@ import { connect } from 'react-redux';
 import { fetchPosts } from '../actions'; //action creator
 
 class PostsIndex extends Component {
+    // This function will be call by React immediately
+    // after this component has shown up in the DOM.
+
+    // It does not matter whether using WillMount or DidMount
+    // because we are doing async stuff inside the lifecyle method.
+    // And the nature of React is React will re-render
+    // as soon as it can render something, which means when the
+    // async stuff is resolved, React will re-render this component.
+    componentDidMount() {
+
+    }
+
     render() {
         return (
             <div>
