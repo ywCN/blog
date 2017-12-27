9c63085cd9dce74e67549f753ae5e21a9d2b65d9
ywCN <wangyouhao90@gmail.com>
2017-12-26 20:45:08 -0500

    play with Router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+    render() { return <div>Hello!</div> }
+}
+
+class Bye extends React.Component {
+    render() { return <div>Bye!</div> }
+}
+
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        {/* <App /> */}
+        <BrowserRouter>
+            <div>
+                {/* path and component are required for Route */}
+                <Route path="/hello" component={Hello} />
+                <Route path="/bye" component={Bye} />
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.container')
+);
