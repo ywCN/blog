007cbd04e0ae835b623bb941dcc5a0cf651d32d8
ywCN <wangyouhao90@gmail.com>
2017-12-26 22:34:27 -0500

    import and combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-    
+    post: PostsReducer
 });
 
 export default rootReducer;
