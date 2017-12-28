1a0be4b51278580766e7c1c5d874d8c890c1af9b
ywCN <wangyouhao90@gmail.com>
2017-12-27 19:22:28 -0500

    comment about 'reducer'

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
 // redux-form handles any type of form you put in Redux.
 // including validaing input and submitting form in a certain way.
+// https://redux-form.com/7.2.0/examples/
+// The 'reducer' is already made by the library.
+// But we need to assign it an alias to make it specific.
 import { reducer as formReducer } from "redux-form";
 import PostsReducer from "./reducer_posts";
 
