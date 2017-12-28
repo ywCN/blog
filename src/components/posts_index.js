66cf41cf9757ec66665f4941b2f4a718e8acf80e
ywCN <wangyouhao90@gmail.com>
2017-12-27 19:10:29 -0500

    comment about Link and to

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -21,6 +21,7 @@ class PostsIndex extends Component {
         return _.map(this.props.posts, post => {
             return (
                 <li className="list-group-item" key={post.id}>
+                    {/* Link is like the <a> in HTML */}
                     <Link to={`/posts/${post.id}`}>
                         {post.title}
                     </Link>
@@ -33,6 +34,7 @@ class PostsIndex extends Component {
         return (
             <div>
                 <div className="text-xs-right">
+                    {/* 'to="/.../..."' will make navigation happen */}
                     <Link className="btn btn-primary" to="/posts/new">
                         Add a Post
                     </Link>
