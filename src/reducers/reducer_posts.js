e6bb8e79d962f79f088d2fe6748afd76589dc44e
ywCN <wangyouhao90@gmail.com>
2017-12-28 15:21:03 -0500

    fetch post reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,9 +5,17 @@ export default function(state = {}, action) {
     switch (action.type) {
         case DELETE_POST:
             return _.omit(state, action.payload);
+        // fetch post and combine all existing posts
+        // this reducer will fetch the post with given ID
         case FETCH_POST:
+            // ES5 
+            // const post = action.payload.data; // get the object need to be appended
+            // const newState = { ...state }; // copy
+            // newState[post.id] = post; // edit copy
+            // return newState; // return copy
+            // ES6
             return { ...state, [action.payload.data.id]: action.payload.data };
-        // This action want to transfer [post1, post2] into {1:post1, 2: post2}
+        // This reducer want to transfer [post1, post2] into {1:post1, 2: post2}
         case FETCH_POSTS:
             // console.log('using FETCH_POSTS');
             // lodash.mapKeys will return a new object 
