0fe865d85aa5e69d67666cd04ba798a8fe6dc7a8
ywCN <wangyouhao90@gmail.com>
2018-01-03 23:15:05 -0500

    https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const FETCH_POSTS = "fetch_posts";
-export const FETCH_POST = "fetch_post";
-export const CREATE_POST = "create_post";
-export const DELETE_POST = "delete_post";
+export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
+export const CREATE_POST = 'create_post';
+export const DELETE_POST = 'delete_post';
 
-const ROOT_URL = "http://reduxblog.herokuapp.com/api";
-const API_KEY = "?key=PAPERCLIP1234";
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=PAPERCLIP1234';
 
 export function fetchPosts() {
     // console.log('using fetchPosts action creator');
